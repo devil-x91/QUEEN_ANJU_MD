e386daf3183bcdbdbf9daa9c99c03ea15b5097b7
LIGHT YAGAMI <164171889+devil-x91@users.noreply.github.com>
2024-11-21 14:31:55 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=TJ83haLZ#ZKC1rA3J2O6Y7fNBDNrqkZIWohvpOrUZdWXZ0xH57Gc",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=nckGwCBD#dGuNzyerxLcdOKgs9Pu13gBFq5dNKl49f8WpevyEFYk",
 };
